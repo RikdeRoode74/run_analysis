03b66342d7b0a8d3131e380fc5d527afb3256a74
Rik de Roode <f.j.deroode@gmail.com>
2018-01-28 21:08:42 +0100

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# run_analysis
-script to download en tidy a phone-dataset
+#CleanPhoneData
+
+CleanPhoneData is a R-script which downloads and tidies a dataset containing measurements of subjects using a a mobile device.
+
+The original dataset can be found here: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+And it's description can be found here: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+The script (CleanPhoneData.R) can be found on github at the following address: https://github.com/RikdeRoode74/run_analysis
+
+# working
+First the dataset is downloaded and unzipped in a working directory
+The dataset is seperated in several related files, only some of which are useful for our purposes.
+The script reads those files, combines and aggregates them to a single dataset which is written back as a simple CSV-file in the working directory named: tidyPhoneData.csv.
+
+Details of the working of the script can be found in comments in the script itself (in combination with the code)
+A description of the resulting file can be found in the CodeBook.md file
---
+++ Run_Analysis.R
@@ -0,0 +1,54 @@
+Run_Analysis <- function()
+{
+  library ("dplyr")
+  #download the dataset en unpack it in a working directory
+  setwd("E:\\R work")
+  if(!file.exists("PhoneData")){
+    dir.create("PhoneData")
+  }
+  setwd("E:\\R work\\PhoneData")
+  
+  filename <- "Dataset.zip"
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile=filename)
+  dateDownloaded <- date()
+  
+  unzip(filename)
+  # get headers from features.txt
+  headers <- read.csv("UCI HAR Dataset\\features.txt", sep=" ", stringsAsFactors=FALSE, header = FALSE)[,2]
+  # find all headers with either mean() of std() in the name
+  relevantHeaders <- grepl("mean()|std()", headers)
+  
+  # read datasets, apply headers and filter on relevant headers
+  trainx <- read.fwf("UCI HAR Dataset\\train\\X_train.txt", widths = rep(16, 561), col.names = headers)[,relevantHeaders]
+  testx <- read.fwf("UCI HAR Dataset\\test\\X_test.txt", widths = rep(16, 561), col.names = headers)[,relevantHeaders]
+
+  # combine train en test sets
+  allx <- bind_rows(trainx, testx)
+  
+  # read and combine subject sets
+  trainsubject <- read.csv("UCI HAR Dataset\\train\\subject_train.txt", header = FALSE)[[1]]
+  testsubject <- read.csv("UCI HAR Dataset\\test\\subject_test.txt", header = FALSE)[[1]]
+  subject <- c(trainsubject, testsubject)
+  # and add it as first row to the dataframe
+  allx <- cbind(subject, allx)
+
+  # read and combine  train en test activity sets
+  trainy <- read.fwf("UCI HAR Dataset\\train\\y_train.txt", widths = rep(1, 1))[[1]]
+  testy <- read.fwf("UCI HAR Dataset\\test\\y_test.txt", widths = rep(1, 1))[[1]]
+  ally <- c(trainy, testy)
+  #transform integers into activities
+  activities <- read.csv("UCI HAR Dataset\\activity_labels.txt", sep=" ", header = FALSE)
+  translationVector <- activities[[2]]
+  names(translationVector) <- activities[[1]]
+  activity <- translationVector[ally]
+  # and add it as first row to the dataframe
+  allx <- cbind(activity, allx)
+  
+  # group by activity and subject and then take means
+  result <- allx %>%
+    group_by(activity, subject) %>%
+    summarise_all(mean)
+  
+  # write it all to 1 CSV
+  write.csv(result, file = "tidyPhoneData.csv")
+}
